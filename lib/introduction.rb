fdd568be8e480851dfaafe12da430c55a261f040
Alexandra Fren <alexandra.fren@gmail.com>
2018-06-05 19:41:42 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hello, #{name} it's nice to meet you!"
 end
 
-def introduction_with_language(name, langauge)
+def introduction_with_language(name, language)
   puts "#{name} speaks #{language}"
 end
